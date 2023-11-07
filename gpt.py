dac7e3a5744d5b68e0ad57c45015d17f8cb5c574
Prerana <prerana@hellosivi.com>
2023-11-07 21:18:21 +0530

    gpt model

--- gpt.py
+++ gpt.py
@@ -1,4 +1,6 @@
 import torch.nn as nn
+import torch
+import torch.nn.functional as F
 
 class FeedFoward(nn.Module):
     def __init__(self, embedding_dim):
@@ -14,20 +16,65 @@ class FeedFoward(nn.Module):
         return x
     
 class Head(nn.Module):
+    def __init__(self, embedding_dim, head_size):
+        super(Head, self).__init__()
+        self.key = nn.Linear(embedding_dim, head_size)
+        self.query = nn.Linear(embedding_dim, head_size)
+        self.value = nn.Linear(embedding_dim, head_size)
+        self.dropout = nn.Dropout(0.1)
+
+    def forward(self, x):
+        query = self.query(x)
+        key = self.key(x)
+        weights = query @ key.transpose(-2,-1) * key.shape[-1]**-0.5
+        mask = torch.triu(torch.ones(x.size(1), x.size(1)), diagonal=1)
+        weights = weights.masked_fill(mask==0, float('-inf'))
+        weights = F.softmax(weights, dim=1)
+        weights = self.dropout(weights)
+        value = self.value(x)
+        weights = weights @ value.T
+        return weights
 
 
+class MultiHeadAttention(nn.Module):
+    def __init__(self, embedding_dim, num_head, d_model) -> None:
+        super().__init__()
+        head_size = d_model // num_head
+        self.heads = nn.ModuleList([Head(embedding_dim, head_size) for _ in range(num_head)])
+        self.proj = nn.Linear(d_model, embedding_dim)
 
+    def forward(self, x):
+        heads = torch.cat([head(x) for head in self.heads], dim=-1)
+        projection = self.proj(heads)
+        return projection
+    
 class Block(nn.Module):
-    def __init__(self, embedding_dim, head_size, num_heads):
+    def __init__(self, embedding_dim, d_model, num_heads):
         super(Block, self).__init__()
         self.ln1 = nn.LayerNorm(embedding_dim)
         self.ln2 = nn.LayerNorm(embedding_dim)
         self.ff = FeedFoward(embedding_dim) 
+        self.sa = MultiHeadAttention(embedding_dim, num_heads, d_model)
 
+    def forward(self, x):
+        x = x + self.sa(self.ln1(x))
+        x = x + self.ff(self.ln2(x))
+        return x
 
 class GPTModel(nn.Module):
-    def __init__(self, vocab_size, embedding_dim, block_size):
+    def __init__(self, vocab_size, embedding_dim, block_size, d_model, num_heads, num_layers):
         super(GPTModel, self).__init__()
         self.embedding = nn.Embedding(vocab_size, embedding_dim)
         self.pos_embedding = nn.Embedding(block_size, embedding_dim)
-        self.blocks = Blocks()
+        self.blocks = nn.Sequential(*[Block(embedding_dim, d_model, num_heads) for _ in range(num_layers)])
+        self.layer_norm = nn.LayerNorm(embedding_dim)
+        self.lm_head = nn.Linear(embedding_dim, vocab_size, bias=False)
+
+    def forward(self, x):
+        x = self.embedding(x)
+        pos = torch.arange(x.size(1), device=x.device).unsqueeze(0)
+        x = x + self.pos_embedding(pos)
+        x = self.blocks(x)
+        x = self.layer_norm(x)
+        x = self.lm_head(x)
+        return x
