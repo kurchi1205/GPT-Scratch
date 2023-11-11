ed67a05d08243077deaf774d43558bbcf92c789b
Prerana <prerana@hellosivi.com>
2023-11-11 07:35:25 +0530

    training model with validation

--- train.py
+++ train.py
@@ -2,6 +2,7 @@ import torch
 from gpt import GPTModel
 from tqdm import tqdm
 import json
+import os
 from data import get_data, get_batch
 
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -27,22 +28,22 @@ def train(cfg, learning_rate, max_iters, eval_interval):
     model = get_model(cfg)
     model = model.to(DEVICE)
     optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
+    if not os.path.exists("models"):
+        os.makedirs("models")
 
     for iter in tqdm(range(max_iters)):
-        # every once in a while evaluate the loss on train and val sets
-        # if iter % cfg["eval_interval"] == 0 or iter == max_iters - 1:
-        #     losses = estimate_loss()
-        #     print(f"step {iter}: train loss {losses['train']:.4f}, val loss {losses['val']:.4f}")
         if iter % cfg["save_interval"] == 0 or iter == max_iters - 1:
-            torch.save(model, f"gpt_trained_{iter}.pth")
-        # sample a batch of data
+            torch.save(model, f"models/gpt_trained_{iter}.pth")
         xb, yb = get_batch(train_data, cfg["block_size"], cfg["batch_size"])
-        # evaluate the loss
         logits, loss = model(xb, yb)
-        if iter % 1000 == 0:
-            print(f"Training Loss: {loss} at iter {iter}")
+        if iter % eval_interval == 0:
+            with torch.no_grad():
+                xb_val, yb_val = get_batch(val_data, cfg["block_size"], cfg["batch_size"])
+                logits_val, loss_val = model(xb_val, yb_val)
+                print(f"Training Loss: {loss} at iter {iter}")
+                print(f"Validation Loss: {loss_val} at iter {iter}")
         optimizer.zero_grad(set_to_none=True)
         loss.backward()
         optimizer.step()
-    torch.save(model, f"gpt_trained.pth")
+    torch.save(model, f"models/gpt_trained.pth")
 
